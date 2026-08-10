d79d5f61699c1daed06969662964a4b58a64a08f
trustscorellm-dotcom <trustscore.llm@gmail.com>
2026-08-10 14:54:23 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   agentRules: false,
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
